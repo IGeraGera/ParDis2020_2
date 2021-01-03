2a504bbab3a42fff46bc6bf7d32cda525fced097
IGeraGera <gerasimoi@ece.auth.gr>
2021-01-04 01:50:22 +0200

    V0 kNN function with block feature

--- V0/main.c
+++ V0/main.c
@@ -27,7 +27,7 @@ typedef struct knnresult{
 */
 
 /* Functions Declaration */
-//knnresult kNN(double * X, double * Y, int n, int m, int d, int k);
+knnresult kNN(double * X, double * Y, int n, int m, int d, int k);
 void checkArgsNum(int argc);
 int getAttributes(FILE *fp);
 double ** readCSV(FILE *fp,double **data, int *dataRows);
@@ -76,43 +76,6 @@ main(int argc, char *argv[]){
 			Y[d*i+j]=data[i+n][j];
 		}
 	}
-	/* Starting Procces */
-	double *X_HAD   = (double *)malloc(n*d*sizeof(double));
-	double *Y_HAD   = (double *)malloc(m*d*sizeof(double));
-	double *e_d_yt  = (double *)malloc(d*m*sizeof(double));
-	double *e_x_dt  = (double *)malloc(n*d*sizeof(double));
-	double *X_part  = (double *)malloc(n*m*sizeof(double));
-	double *Y_part  = (double *)malloc(n*m*sizeof(double));
-	double *XY_part = (double *)malloc(n*m*sizeof(double));
-	double *D       = (double *)malloc(n*m*sizeof(double));
-	int    *idx     = (int *)   malloc(m*k*sizeof(int));
-	double *dist    = (double *)malloc(m*k*sizeof(double));
-	struct knnresult result;
-
-	/* Init dist */
-	for (int i=0;i<m*k;i++){
-		dist[i]=0;
-		idx[i]=-1;
-	}
-
-	/* X Hadamart */
-	for (int i=0;i<n*d;i++){
-		X_HAD[i]=X[i]*X[i];
-		e_x_dt[i]=1.0;
-	}
-	/* X Hadamart */
-	for (int i=0;i<m*d;i++){
-		Y_HAD[i]=Y[i]*Y[i];
-		e_d_yt[i]=1.0;
-	}
-	/* Main CBLAS part */
-	cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,n,m,d,1.0,X_HAD,d,e_d_yt,m,0,X_part,m);
-	cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasTrans,n,m,d,1.0,e_x_dt,d,Y_HAD,d,0,Y_part,m);
-	cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasTrans,n,m,d,2.0,X,d,Y,d,0,XY_part,m);
-	/* D = sqrt(Xpart - XYpart - Ypart) */	
-	for (int i=0;i<n*m;i++){
-		D[i]=sqrt(X_part[i] - XY_part[i]  + Y_part[i]);
-	}
 	/* Prints D  */
 	/*
 	for (int i= 0 ; i<n; i++){
@@ -123,39 +86,12 @@ main(int argc, char *argv[]){
 	}
 	puts("");
 	*/	
-	/* Find the k nearest neighbours */
-	for (int j=0;j<n;j++){
-		for (int i=0;i<m;i++){
-			
-			for (int knn=0;knn<k;knn++){
-				if (D[j*m+i] >= dist[i*k+knn]){
-					double tmpdist = dist[i*k+knn];
-					int tmpidx = idx[i*k+knn];
-					dist[i*k+knn] = D[j*m+i] ;
-					idx[i*k+knn] = j*m+i;
-					/* If k index (knn) is 0 there is no place to put the tmp so break  */
-					if (knn!=0){
-						dist[i*k+knn-1] = tmpdist;
-						idx[i*k+knn-1] = tmpidx;
-					}
-				}
-			}
-		}
-	}
-	/*	
+
+	struct knnresult res=kNN(X,Y,n,m,d,k);
+
 	for (int i = 0 ; i<m*k;i++)
-		printf("dist: %lf \t index: %d  \n",dist[i],idx[i]);
-	*/
+		printf("dist: %lf \t index: %d  \n",res.ndist[i],res.nidx[i]);
 
-
-	free(X_HAD);	
-	free(Y_HAD);	
-	free(e_d_yt);	
-	free(e_x_dt);	
-	free(X_part);
-	free(Y_part);
-	free(XY_part);
-	free(D);
 	/*Ending Proccess  */
 	free(X);
 	free(Y);
@@ -163,6 +99,110 @@ main(int argc, char *argv[]){
 	fclose(fp);
 	exit(EXIT_SUCCESS);
 
+}
+
+knnresult
+kNN(double * X, double * Y, int n, int m, int d, int k){
+	# define numOfBlocks 8
+	/* knnresult struct variables */
+	int    *idx     = (int *)   malloc(m*k*sizeof(int));
+	double *dist    = (double *)malloc(m*k*sizeof(double));
+	/* Initialize dist and idx */
+	for (int i=0;i<m*k;i++){
+		dist[i]=0;
+		idx[i]=-1;
+	}
+	/* Variables that only allocate once */
+	double *X_HAD   = (double *)malloc(n*d*sizeof(double));
+	double *e_x_dt  = (double *)malloc(n*d*sizeof(double));
+	/* Initialize X Hadamart and e_x_dt */
+	for (int i=0;i<n*d;i++){
+		X_HAD[i]=X[i]*X[i];
+		e_x_dt[i]=1.0;
+	}
+	/* Block the Y */
+	for (int current_block=1; current_block <= numOfBlocks; current_block++){
+		/* Block variables */
+		int mBlock = m / numOfBlocks;
+		int mIndex = mBlock * (current_block-1);
+		if (current_block == numOfBlocks)
+			mBlock= m - (numOfBlocks-1) * mBlock;
+		/* Main variables */
+		double *Y_HAD   = (double *)malloc(mBlock*d*sizeof(double));
+		double *e_d_yt  = (double *)malloc(d*mBlock*sizeof(double));
+		double *X_part  = (double *)malloc(n*mBlock*sizeof(double));
+		double *Y_part  = (double *)malloc(n*mBlock*sizeof(double));
+		double *XY_part = (double *)malloc(n*mBlock*sizeof(double));
+		double *D       = (double *)malloc(n*mBlock*sizeof(double));
+		/* Initialize Y Hadamart */
+		for (int i=0;i<mBlock*d;i++){
+			Y_HAD[i]=Y[mIndex*d+i]*Y[mIndex*d+i];
+			e_d_yt[i]=1.0;
+		}
+		/* Main CBLAS part */
+		cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,n,mBlock,d,1.0,X_HAD,d,e_d_yt,mBlock,0,X_part,mBlock);
+		cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasTrans,n,mBlock,d,1.0,e_x_dt,d,Y_HAD,d,0,Y_part,mBlock);
+		cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasTrans,n,mBlock,d,2.0,X,d,&Y[mIndex*d],d,0,XY_part,mBlock);
+		/* D = sqrt(Xpart - XYpart - Ypart) */	
+		for (int i=0;i<n*mBlock;i++){
+			D[i]=sqrt(X_part[i] - XY_part[i]  + Y_part[i]);
+		}
+		/* Prints D  */
+		/*
+		for (int i= 0 ; i<n; i++){
+			for (int j=0; j<mBlock ; j++){
+				printf("%lf ",D[mBlock*i+j]);
+			}
+			puts("");
+		}
+		puts("");
+		*/
+
+		/* Find the k nearest neighbours */
+		for (int j=0;j<n;j++){
+			for (int i=0;i<mBlock;i++){
+				/* Check if D is greater than dist for every k  */
+				for (int knn=0;knn<k;knn++){
+					if (D[j*mBlock+i] >= dist[(i+mIndex)*k+knn]){
+						double tmpdist = dist[(i+mIndex)*k+knn];
+						int tmpidx = idx[(i+mIndex)*k+knn];
+						dist[(i+mIndex)*k+knn] = D[j*mBlock+i] ;
+						idx[(i+mIndex)*k+knn] = (j*m+(i+mIndex));
+						/* If k index (knn) is 0 there is no place to put the tmp so break  */
+						if (knn!=0){
+							dist[(i+mIndex)*k+knn-1] = tmpdist;
+							idx[(i+mIndex)*k+knn-1] = tmpidx;
+						}
+					}
+				}
+			}
+		}
+		/* Free Block variables */
+		free(X_part);
+		free(Y_part);
+		free(XY_part);
+		free(e_d_yt);	
+		free(Y_HAD);	
+		free(D);
+
+		//current_block++;
+	}
+	/* Prinf dist and idx  */
+	/*
+	for (int i = 0 ; i<m*k;i++)
+		printf("dist: %lf \t index: %d  \n",dist[i],idx[i]);
+	*/
+	/* Set up knnresult  */
+	struct knnresult result;
+	result.m = m;
+	result.k = k;
+	result.ndist = dist;
+	result.nidx = idx;
+
+	free(X_HAD);	
+	free(e_x_dt);	
+
+	return result;
 }
 
 void
@@ -184,10 +224,9 @@ getAttributes(FILE *fp){
 	char buf[2048];
 	int attributes=0;
 	char delim=',';
-	/* Gete first line from file */
-	fgets(buf,2048,fp);
-	/* check if file is empty */
-	if (buf==NULL){
+	/* Get first line from file */
+	/* and check if file is empty */
+	if (fgets(buf,2048,fp)==NULL){
 		fprintf(stderr,"Line %d: Input file empty\n",__LINE__ );
 		exit(EXIT_FAILURE);
 	}
--- datascripts/matlab_D.m
+++ datascripts/matlab_D.m
@@ -5,11 +5,13 @@
 data = importdata("../Datasets/BreastCancerCoimbra.csv");
 dataRows =size(data)(1);
 d =size(data)(2);
-n=floor(dataRows*0.75);
+n=floor(dataRows*0.5);
 m=dataRows-n;
 
 X=data(1:n,:);
 Y=data(n+1:end,:);
 
+Y=Y(:,:)
 
 D = sqrt(sum(X.^2,2) - 2 * X*Y.' + sum(Y.^2,2).')
+
