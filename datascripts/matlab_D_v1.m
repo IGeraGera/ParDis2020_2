e2b3d91b25190caf93394cf5d9480d767e37533b
IGeraGera <gerasimoi@ece.auth.gr>
2021-01-08 01:42:02 +0200

    Added matlab testing scripts for V1

--- datascripts/matlab_D_v1.m
+++ datascripts/matlab_D_v1.m
@@ -3,15 +3,12 @@
 # Get data
 data = importdata("../Datasets/Container_Crane_Controller_Data_Set.csv");
 #data = importdata("../Datasets/BreastCancerCoimbra.csv");
-dataRows =size(data)(1);
+n =size(data)(1);
 d =size(data)(2);
-n=floor(dataRows*0.5);
-m=dataRows-n;
 
-X=data(1:n,:);
-Y=data(n+1:end,:);
 
-Y=Y(:,:)
+Y=data;
+X=data;
 
 D = sqrt(sum(X.^2,2) - 2 * X*Y.' + sum(Y.^2,2).')
 
